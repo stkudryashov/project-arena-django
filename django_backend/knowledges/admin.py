cd0b4e547bb1f4caded8d94a0c00bf1b213f31e7
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-06-03 23:03:48 +0300

    Дополнительные поля справочника

--- django_backend/knowledges/admin.py
+++ django_backend/knowledges/admin.py
@@ -5,4 +5,26 @@ from knowledges.models import Knowledge
 
 @admin.register(Knowledge)
 class KnowledgeAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        ('Локализация', {
+            'fields': ('language',)
+        }),
+        ('Регистрация', {
+            'fields': ('reg_phone_number', 'reg_username', 'reg_date_of_birth', 'reg_city', 'reg_level', 'reg_playtime')
+        }),
+        ('Ошибки ввода регистрации', {
+            'fields': ('error_phone_number', 'error_username', 'error_date_of_birth')
+        }),
+        ('Уровни игры (через запятую)', {
+            'fields': ('play_skill_params',)
+        }),
+        ('Эмодзи выбора времени', {
+            'fields': ('time_unselect_emoji', 'time_select_emoji')
+        }),
+        ('Главное меню', {
+            'fields': ('btn_future_games', 'btn_my_games', 'btn_friends', 'btn_profile', 'btn_notifications')
+        })
+    )
+
+    list_display = ('language',)
+    list_display_links = ('language',)
--- django_backend/knowledges/models.py
+++ django_backend/knowledges/models.py
@@ -13,6 +13,21 @@ class Knowledge(models.Model):
     reg_level = models.CharField(max_length=128, verbose_name='Запрос уровня игры')
     reg_playtime = models.CharField(max_length=128, verbose_name='Запрос времени игры')
 
+    error_phone_number = models.CharField(max_length=128, verbose_name='Ошибка номера телефона')
+    error_username = models.CharField(max_length=128, verbose_name='Ошибка имени и фамилии')
+    error_date_of_birth = models.CharField(max_length=128, verbose_name='Ошибка даты рождения')
+
+    play_skill_params = models.CharField(max_length=128, verbose_name='Уровни игры')
+
+    time_unselect_emoji = models.CharField(max_length=128, verbose_name='Эмодзи (не выбрано)')
+    time_select_emoji = models.CharField(max_length=128, verbose_name='Эмодзи (выбрано)')
+
+    btn_future_games = models.CharField(max_length=128, verbose_name='Кнопка "Предстоящие игры"')
+    btn_my_games = models.CharField(max_length=128, verbose_name='Кнопка "Мои игры"')
+    btn_friends = models.CharField(max_length=128, verbose_name='Кнопка "Друзья"')
+    btn_profile = models.CharField(max_length=128, verbose_name='Кнопка "Профиль"')
+    btn_notifications = models.CharField(max_length=128, verbose_name='Кнопка "Уведомления"')
+
     def __str__(self):
         return f'{self.language}'
 
