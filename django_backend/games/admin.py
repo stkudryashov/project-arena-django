4b77089500735f76248e72a57a5d57d6818317e8
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-06-11 08:38:20 +0300

    Список игроков в панели администратора

--- django_backend/games/admin.py
+++ django_backend/games/admin.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 from games.models import Game, TelegramUserGame
 
 
+class TelegramUserGameInLine(admin.TabularInline):
+    model = TelegramUserGame
+    verbose_name_plural = 'Игроки'
+
+
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     list_display = ('datetime', 'arena', 'status')
+    inlines = [TelegramUserGameInLine]
 
 
 @admin.register(TelegramUserGame)
