b8743119650e98bf4eab6c68b5f00b4d383e602d
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-06-27 15:58:40 +0400

    Слияние веток

--- django_backend/games/models.py
+++ django_backend/games/models.py
@@ -10,8 +10,10 @@ import json
 
 
 class Game(models.Model):
+    """Модель игры привязанной к определенному манежу"""
+
     datetime = models.DateTimeField(verbose_name='Дата')
-    max_players = models.PositiveIntegerField(verbose_name='Количество мест')
+    max_players = models.PositiveIntegerField(verbose_name='Количество мест', default=0)
     price = models.PositiveIntegerField(verbose_name='Стоимость участия')
 
     arena = models.ForeignKey(Arena, on_delete=models.PROTECT, related_name='games', verbose_name='Манеж')
@@ -42,6 +44,27 @@ class Game(models.Model):
             one_off=True
         )
 
+    @property
+    def free_space(self):
+        """Число свободных мест в игре"""
+
+        space = self.max_players - self.players.all().count()
+        return space if space > 0 else 0
+
+    free_space.fget.short_description = 'Свободно мест'
+
+    @property
+    def has_space(self):
+        """Есть ли свободные места в игре"""
+
+        return self.free_space > 0
+
+    # @property
+    # def is_end(self):
+    #     """Закончилась ли игра"""
+    #
+    #     return self.datetime < datetime.now()
+
     def __str__(self):
         return f'{self.datetime} - {self.arena}'
 
@@ -51,6 +74,8 @@ class Game(models.Model):
 
 
 class TelegramUserGame(models.Model):
+    """Промежуточная модель между пользователем и игрой"""
+
     user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE, blank=True,
                              related_name='games', verbose_name='Пользователь')
 
@@ -63,7 +88,7 @@ class TelegramUserGame(models.Model):
         ('refused', 'Отказался'),
     )
 
-    status = models.CharField(choices=PLAYER_STATUS, max_length=32, verbose_name='Статус')
+    status = models.CharField(choices=PLAYER_STATUS, max_length=32, verbose_name='Статус', default=PLAYER_STATUS[0][0])
 
     def __str__(self):
         return f'{self.user.telegram_username} - {self.game.arena} {self.game.datetime}'
